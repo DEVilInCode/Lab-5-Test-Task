c4a3bd7b5fb01cca1083f09c1d9fe349c07e7805
DEVilCode <tsgoevr@mail.ru>
2025-03-24 15:40:28 +0300

    Implement session service

--- Lab5TestTask/Services/Implementations/SessionService.cs
+++ Lab5TestTask/Services/Implementations/SessionService.cs
@@ -20,11 +20,15 @@ public class SessionService : ISessionService
 
     public async Task<Session> GetSessionAsync()
     {
-        throw new NotImplementedException();
+        return await _dbContext.Sessions
+            .OrderBy(s => s.StartedAtUTC)
+            .FirstOrDefaultAsync();
     }
 
     public async Task<List<Session>> GetSessionsAsync()
     {
-        throw new NotImplementedException();
+        return await _dbContext.Sessions
+            .Where(s => s.User.Status == Enums.UserStatus.Active && s.EndedAtUTC < new DateTime(2025, 1, 1))
+            .ToListAsync();
     }
 }
